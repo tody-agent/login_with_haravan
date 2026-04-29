efbe953a127c6e41f149d9ebc3e1d94b896ec0b9
tody-agent <tracuuphat@gmail.com>
2026-04-29 22:45:38 +0700

    chore: bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="login_with_haravan",
-    version="0.1.3",
+    version="0.1.4",
     description="Login with Haravan Account for Frappe Helpdesk",
     author="Haravan",
     packages=find_packages(),
